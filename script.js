76df906068bd41929353152c3d97834d9e896469
Munashe Stabnashia Nzira <93272195+stabnashiamunashe@users.noreply.github.com>
2023-12-07 16:28:04 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Function to show the popup
+function showPopup() {
+    var popup = document.getElementById('customPopup');
+    popup.style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+    var popup = document.getElementById('customPopup');
+    popup.style.display = 'none';
+}
+
+// Example: Listen for a custom event named "customPopupEvent"
+document.addEventListener('customPopupEvent', function () {
+    // Code to show the popup
+    showPopup();
+});
