7dea5decfe2240c13106bf37d1032b8895f9f6b4
Munashe Stabnashia Nzira <93272195+stabnashiamunashe@users.noreply.github.com>
2024-02-05 06:58:17 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,23 +1,18 @@
-// Function to show the modal overlay
 function showOverlay() {
     var overlay = document.getElementById('customPopupOverlay');
-    overlay.style.display = 'flex'; // Use 'flex' to center the modal content
+    overlay.style.display = 'flex'; 
 }
 
-// Function to close the modal overlay
 function closeOverlay() {
     var overlay = document.getElementById('customPopupOverlay');
     overlay.style.display = 'none';
 }
 
-// Example: Listen for a custom event named "customPopupEvent"
 document.addEventListener('customPopupEvent', function () {
-    // Code to show the modal overlay
     console.log("Custom Event Listener Registered: For Testing PopUps") 
     showOverlay();
 });
 
-// Dispatch the custom event when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
     document.dispatchEvent(new Event('customPopupEvent'));
     console.log("Custom Event Triggered: For Testing PopUps")
